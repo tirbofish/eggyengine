4d1919ebc933407c3a001b2f02a6f1cf117d7dee
Thribhu K <4tkbytes@pm.me>
2026-04-15 19:51:22 +1000

    wip

--- build.zig
+++ build.zig
@@ -4,30 +4,62 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const eggy = buildEggyLibrary(b, target, optimize);
+    buildExecutable(b, target, optimize, eggy);
+}
+
+fn buildEggyLibrary(
+    b: *std.Build,
+    target: std.Build.ResolvedTarget,
+    optimize: std.builtin.OptimizeMode,
+) *std.Build.Module {
+    const sdl3 = b.dependency("sdl3", .{
+        .target = target,
+        .optimize = optimize,
+        .c_sdl_preferred_linkage = .static,
+    });
+
+    const vulkan = b.dependency("vulkan", .{
+        .registry = b.path("deps/vk.xml"),
+    }).module("vulkan-zig");
+
+    const eggy_module = b.createModule(.{
+        .root_source_file = b.path("src/engine/eggy.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    eggy_module.addImport("vulkan", vulkan);
+    eggy_module.addImport("sdl3", sdl3.module("sdl3"));
+
+    return eggy_module;
+}
+
+fn buildExecutable(
+    b: *std.Build,
+    target: std.Build.ResolvedTarget,
+    optimize: std.builtin.OptimizeMode,
+    eggy: *std.Build.Module,
+) void {
     const root_module = b.createModule(.{
         .root_source_file = b.path("src/game/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
+    root_module.addImport("eggy", eggy);
+
     const exe = b.addExecutable(.{
         .name = "eggyengine-demo",
         .root_module = root_module,
     });
-
-    const sdl3 = b.dependency("sdl3", .{
-        .target = target,
-        .optimize = optimize,
-    });
-
-    root_module.addImport("sdl3", sdl3.module("sdl3"));
 
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
 
-    // add args like `zig build run -- arg1 arg2 etc`. 
+    // allows for args like `zig build run -- arg1 arg2 etc`.
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
---
+++ src/engine/eggy.zig
@@ -0,0 +1,16 @@
+// const sdl = @import("sdl3");
+// const vk = @import("vulkan");
+
+const std = @import("std");
+
+pub const EggyApp = struct {
+    
+
+    pub fn init() !EggyApp {
+        
+    }
+
+    pub fn deinit(_: *EggyApp) void {
+
+    }
+};
